425c20a74f7748f8da436123fa0bdd638b768600
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-08-13 01:31:29 +0300

    Add build folder

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,5 @@
 git clean -df                                                     &&
 conan remove -f '*'                                               &&
 conan install -pr:b=default -if ../conanfiles --build=missing ../ &&
-conan build -if ../conanfiles -bf ../ ../                         &&
-../conan_test
+conan build -if ../conanfiles -bf ../build ../                    &&
+../build/conan_test
