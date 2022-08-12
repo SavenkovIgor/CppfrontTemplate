72d07fc92778c614a756d881810515ca64ad11af
Savenkov Igor <savenkovigor.dev@gmail.com>
2022-08-12 12:59:31 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,46 +1,21 @@
 from conans import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMakeDeps #, CMake
+from conan.tools.cmake import CMakeToolchain, CMakeDeps
 
 
 class TermGraphConan(ConanFile):
-
-    generators = 'CMakeToolchain' #, 'CMakeDeps'
+    generators = 'CMakeToolchain', 'CMakeDeps'
     settings = 'os', 'arch', 'compiler', 'build_type'
 
     requires = 'fmt/9.0.0'
 
     def generate(self):
-        tc = CMakeToolchain(self)
-        tc.generate()
-
         deps = CMakeDeps(self)
         deps.generate()
+
+        tc = CMakeToolchain(self)
+        tc.generate()
 
     # def build(self):
         # cmake = CMake(self)
         # cmake.configure()
         # cmake.build()
-
-
-# from conans import ConanFile
-# from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
-
-
-# class TermGraphConan(ConanFile):
-
-#     generators = 'CMakeToolchain', 'CMakeDeps'
-#     settings = 'os', 'arch', 'compiler', 'build_type'
-
-#     requires = 'outcome/2.2.3'
-
-#     def generate(self):
-#         tc = CMakeToolchain(self)
-#         tc.generate()
-
-#         deps = CMakeDeps(self)
-#         deps.generate()
-
-#     def build(self):
-#         cmake = CMake(self)
-#         cmake.configure()
-#         cmake.build()
